0fe2c788adf511defae17a5920bf88a6005f1ffe
احمد شاكر <88011946+Ah95ed@users.noreply.github.com>
2026-09-17 20:27:57 +0300

    Update main
    None

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,8 @@ import 'package:ishara/views/home_view.dart';
 import 'package:provider/provider.dart';
 
 Future<void> main() async {
+  //main fun
+  // here no thiing 
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const IsharaApp());
 }
